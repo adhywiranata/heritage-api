5e85e22006b3e86b11f3f9b7b3215e97a7fae2fc
Adhy <adhywiranatap@gmail.com>
2017-03-14 19:00:01 +0700

    added morgan for logging

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const routes = require('./routes/index.route');
 
 const app = express();
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.use('/api', routes);
 
 app.listen(1235);
-// console.log('listening on port 1235');
