42ca1c46feda376a73c7e5e5f149faa4fc5c6d85
Adhy <adhywiranatap@gmail.com>
2017-03-14 20:44:28 +0700

    updated routing for users

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const routes = require('./routes/index.route');
 
@@ -8,6 +9,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost/heritageDB');
+const db = mongoose.connection;
+db.on('error', () => {});
+db.once('open', () => {});
 
 app.use('/api', routes);
 
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+const usersRoutes = require('./users.route');
 const placesRoutes = require('./places.route');
 
 const router = express.Router();
 
+router.use('/users', usersRoutes);
 router.use('/places', placesRoutes);
 
 module.exports = router;
