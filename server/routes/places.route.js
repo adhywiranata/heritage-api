0149f76071467cc27eb50a23e4f8a1195245ac4c
Adhy <adhywiranatap@gmail.com>
2017-03-14 18:22:59 +0700

    add post route for create new place

--- server/routes/places.route.js
+++ server/routes/places.route.js
@@ -3,7 +3,9 @@ const PlaceController = require('../controllers/place.controller');
 
 const router = express.Router();
 
-router.route('/').get(PlaceController.getPlaces);
+router.route('/')
+  .get(PlaceController.getPlaces)
+  .post(PlaceController.createNewPlace);
 router.route('/:id').get(PlaceController.getPlaceById);
 
 module.exports = router;
