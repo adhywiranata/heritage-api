dd32ee35a8b37a8ee37580cc0fd6c8c190bbc9e4
Adhy <adhywiranatap@gmail.com>
2017-03-14 18:33:20 +0700

    added create new place from request body

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index.route');
 
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.use('/api', routes);
 
 app.listen(1235);
