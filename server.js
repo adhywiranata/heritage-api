bfdb5512b60e08942fb563dbea8caf00da954d1a
Adhy <adhywiranatap@gmail.com>
2017-03-14 13:38:23 +0700

    added mongoose connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var placesRoute = require('./routes/places');
 
 var app = express();
@@ -9,3 +11,12 @@ app.get('/places/:id', placesRoute.findById);
 
 app.listen(1234);
 console.log('listening on port 1234');
+
+mongoose.connect('mongodb://localhost/heritageDB');
+var db = mongoose.connection;
+db.on('error', () => {
+  console.log('err');
+});
+db.once('open', () => {
+  console.log('connected to heritageDB');
+});
