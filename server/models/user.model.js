8fdc691fa26ec8275e2ec1a7ddb5c1dcf8538a82
Adhy <adhywiranatap@gmail.com>
2017-03-14 21:07:00 +0700

    added select false option to password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   firstName: { type: String, required: true },
   lastName: { type: String, default: '' },
   description: { type: String, default: '' },
