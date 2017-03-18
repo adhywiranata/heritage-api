7c089e942477ec9646e834167f256ccd8539dd96
Adhy <adhywiranatap@gmail.com>
2017-03-18 15:07:12 +0700

    modularized auth check function from routes

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,12 +1,6 @@
-/* eslint-disable consistent-return, no-param-reassign  */
-
 const express = require('express');
-const jwt = require('jsonwebtoken');
 
-const config = require('../../config');
-
-const app = express();
-app.set('superSecret', config.secret);
+const isAuthenticated = require('../helpers/isAuthenticated');
 
 const usersRoutes = require('./users.route');
 const placesRoutes = require('./places.route');
@@ -15,26 +9,7 @@ const authRoutes = require('./auth.route');
 const router = express.Router();
 
 router.use('/auth', authRoutes);
-
-const isAuthenticated = (req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (token) {
-    jwt.verify(token, app.get('superSecret'), (err, decoded) => {
-      if (err) {
-        return res.json({ success: false, message: 'failed to authenticate' });
-      }
-      req.decoded = decoded;
-      next();
-    });
-  } else {
-    return res.status(403).send({
-      success: false,
-      message: 'no token provided',
-    });
-  }
-};
-
 router.use('/users', isAuthenticated, usersRoutes);
-router.use('/places', isAuthenticated, placesRoutes);
+router.use('/places', placesRoutes);
 
 module.exports = router;
